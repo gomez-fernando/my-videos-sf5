8e31aefa040f602ea1addb07eda00752e842ed01
gomez-fernando <ferdgomez@hotmail.es>
2020-08-05 18:07:22 +0200

    creando lso templates del admin area

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_main_page")
+     */
+    public function index()
+    {
+        return $this->render('admin/my_profile.html.twig');
+    }
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories()
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+}
