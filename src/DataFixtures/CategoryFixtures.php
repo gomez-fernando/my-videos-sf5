ff2afe7e87bc8e490bf12a783d253264dde8fe15
gomez-fernando <ferdgomez@hotmail.es>
2020-08-07 23:03:27 +0200

    first fixtures created

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -4,14 +4,56 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Category;
 
 class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadMainCategories($manager);
+        $this->loadSubcategories($manager, 'Electronics', 1);
+    }
+
+    private function loadMainCategories($manager){
+        foreach($this->getMainCategoriesData() as [$name]){
+            $category = new Category();
+            $category->setName($name);
+
+            $manager->persist($category);
+        }
 
         $manager->flush();
+    }
+
+    private function loadSubcategories($manager, $category, $parent_id){
+        $parent = $manager->getRepository(Category::class)->find($parent_id);
+        $methodName = "get{$category}Data";
+
+        foreach($this->$methodName() as [$name]){
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+
+    private function getMainCategoriesData(){
+        return [
+            ['Electronics', 1],
+            ['Scary', 2],
+            ['Unbelievable', 3],
+            ['Inspirational', 4],
+        ];
+    }
+
+    private function getElectronicsData(){
+        return [
+            ['Cameras', 5],
+            ['Computers', 6],
+            ['Cell Phones', 7],
+        ];
     }
 }
