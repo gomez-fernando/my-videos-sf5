d6265f631cc9163e4a4f852054922c713273717f
gomez-fernando <ferdgomez@hotmail.es>
2020-08-07 19:13:14 +0200

    terminados los templates

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -25,4 +25,36 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/categories.html.twig');
     }
+
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategory()
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
+
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos()
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+
+    /**
+     * @Route("/upload-video", name="upload_video")
+     */
+    public function uploadVideo()
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users()
+    {
+        return $this->render('admin/users.html.twig');
+    }
 }
