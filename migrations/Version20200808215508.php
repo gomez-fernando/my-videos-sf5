5d35eb374a2ff57b528ded1c5098e51c84aad28d
gomez-fernando <ferdgomez@hotmail.es>
2020-08-08 23:56:20 +0200

    first unit test: Slugger

--- migrations/Version20200808215508.php
+++ migrations/Version20200808215508.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200808154040 extends AbstractMigration
+final class Version20200808215508 extends AbstractMigration
 {
     public function getDescription() : string
     {
